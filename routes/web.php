19489c325d2f7c6a02bb4f314a96f7b625b41fc9
Azra <ivana24si@mahasiswa.pcr.ac.id>
2025-09-27 14:47:10 +0700

    azra commit

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\GuestProyekController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Route::get('/guest/proyek', [GuestProyekController::class, 'index'])->name('guest.proyek');
 
